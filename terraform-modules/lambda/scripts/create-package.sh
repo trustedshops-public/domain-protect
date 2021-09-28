d103aa086b8d937eeb21d76685d317818c6d344c
Timo Reymann <mail@timo-reymann.de>
2021-09-28 14:59:06 +0200

    feat: Fail on build error for lambda, Install without venv

--- terraform-modules/lambda/scripts/create-package.sh
+++ terraform-modules/lambda/scripts/create-package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 echo "Executing create_package.sh..."
 
@@ -9,28 +10,20 @@ do
   dir_name=lambda_dist_pkg_$i/
   mkdir -p $path_module/build/$dir_name
 
-  # Create and activate virtual environment...
-  virtualenv -p $runtime env_$i
-  source $path_cwd/env_$i/bin/activate
-
   # Installing python dependencies...
   FILE=$path_module/code/$i/requirements.txt
 
   if [ -f "$FILE" ]; then
     echo "Installing dependencies..."
     echo "From: requirements.txt file exists..."
-    pip install -r "$FILE"
+    pip3 install -r "$FILE" -t $path_module/build/$dir_name/
 
   else
     echo "Error: requirements.txt does not exist!"
   fi
 
-  # Deactivate virtual environment...
-  deactivate
-
   # Create deployment package...
   echo "Creating deployment package..."
-  cp -r $path_cwd/env_$i/lib/$runtime/site-packages/. $path_module/build/$dir_name
   cp $path_module/code/$i/$i.py $path_module/build/$dir_name
 
 # Removing virtual environment folder...
